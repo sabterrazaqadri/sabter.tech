ff7b0102a2cf462e67d6100a68206f5bcee4e1f8
Sabter Iqbal <sabterrazaqadri@gmail.com>
2025-10-28 19:09:54 +0500

    Refactor Homepage component to use Next.js Script

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,47 @@
+"use client";
+
 import { div } from "framer-motion/client";
+import Script from "next/script";
 import HeroSection from "./Components/Herosection";
 import AboutMeSection from "./Components/Aboutme";
 import SkillsSection from "./Components/Skills";
 import ProjectsSection from "./Components/ProjectsSection";
-import { Contact } from "lucide-react";
 import ContactSection from "./Components/Contact";
 
-export default function Homepage(){
-  return(
-  <div>
-  <HeroSection/>
+export default function Homepage() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-between">
+      <HeroSection />
 
-<script type="text/javascript">
-	atOptions = {
-		'key' : '3c1dc3109d1c1cf94cd1a3204ef488c7',
-		'format' : 'iframe',
-		'height' : 90,
-		'width' : 728,
-		'params' : {}
-	};
-</script>
-<script type="text/javascript" src="//www.highperformanceformat.com/3c1dc3109d1c1cf94cd1a3204ef488c7/invoke.js"></script>
-    
-  <AboutMeSection/>   
-  <SkillsSection/>
-  <ProjectsSection/>
-  <ContactSection/>
-  </div>
-  )
+      {/* ✅ Adsterra Ad Script */}
+      <Script
+        id="adsterra-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            atOptions = {
+              'key' : '3c1dc3109d1c1cf94cd1a3204ef488c7',
+              'format' : 'iframe',
+              'height' : 90,
+              'width' : 728,
+              'params' : {}
+            };
+          `,
+        }}
+      />
+      <Script
+        src="//www.highperformanceformat.com/3c1dc3109d1c1cf94cd1a3204ef488c7/invoke.js"
+        strategy="afterInteractive"
+        async
+      />
+
+      {/* Ad container */}
+      <div id="container-3c1dc3109d1c1cf94cd1a3204ef488c7" className="my-6" />
+
+      <AboutMeSection />
+      <SkillsSection />
+      <ProjectsSection />
+      <ContactSection />
+    </div>
+  );
 }
