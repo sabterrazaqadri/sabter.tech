96ce63295e789c75252c3889abe519fa295d4725
Sabter Iqbal <sabterrazaqadri@gmail.com>
2025-10-28 18:55:16 +0500

    Insert ad script into Homepage component
    Added script tags for ad options and invocation.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,18 @@ export default function Homepage(){
   return(
   <div>
   <HeroSection/>
+
+<script type="text/javascript">
+	atOptions = {
+		'key' : '3c1dc3109d1c1cf94cd1a3204ef488c7',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+</script>
+<script type="text/javascript" src="//www.highperformanceformat.com/3c1dc3109d1c1cf94cd1a3204ef488c7/invoke.js"></script>
+    
   <AboutMeSection/>   
   <SkillsSection/>
   <ProjectsSection/>
